4a88284c2e628ba24e567612b41c901fcd8fe536
BLACKEYE-ASTRO <akashshingare2003@gmail.com>
2024-10-16 17:57:38 +0530

    All Updates Done

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
@@ -8,8 +7,10 @@ import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { textVariant } from "../utils/motion";
 
-
 const Tech = () => {
+  // Detect if the user is on a mobile device
+  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
   return (
     <>
       <motion.div variants={textVariant()}>
@@ -18,7 +19,12 @@ const Tech = () => {
       <div className='flex flex-row flex-wrap justify-center gap-10'>
         {technologies.map((technology) => (
           <div className='w-28 h-28' key={technology.name}>
-            <BallCanvas icon={technology.icon} />
+            {/* Render BallCanvas for non-mobile devices, use a simpler image/icon for mobile */}
+            {!isMobile ? (
+              <BallCanvas icon={technology.icon} />
+            ) : (
+              <img src={technology.icon} alt={technology.name} className="w-full h-full object-contain" />
+            )}
           </div>
         ))}
       </div>
