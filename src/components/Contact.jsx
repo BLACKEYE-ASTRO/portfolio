80bfa8cacdb9298bf970268511ca08d0705218e4
BLACKEYE-ASTRO <akashshingare2003@gmail.com>
2024-12-20 21:06:23 +0530

    All Upadated

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -30,11 +30,10 @@ const Contact = () => {
     e.preventDefault();
     setLoading(true);
 
-    // Ensure the values are passed as strings
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID, // Make sure the service ID is a string
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID, // Make sure the template ID is a string
+        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID, 
         {
           from_name: form.name,
           to_name: "Akash Shingare",
